aaf52cf1fb2bb818d4295b43fcb5701b3533cd9e
Franklin Nunez <32471881+abelsinx@users.noreply.github.com>
2018-03-24 15:18:52 -0400

    Add files via upload
    Python Script used to display web service error_logs

---
+++ log_script.py
@@ -0,0 +1,22 @@
+#!usr/bin/python
+import os
+
+f = open("/var/log/httpd/error_log","r")
+of = open("./output_logs", "w")
+num = 1
+
+for line in f.readlines():
+    array = line.split("] ")
+    if(len(array) > 1):
+        date = array[0]
+        source = array[1]
+        reason = array[3]
+        of.write("\nLOG: " + str(num))
+        of.write("\nDate: " + date[1:])
+        of.write("\nSource: " + source[1:])
+        of.write("\nReason: " + reason)
+        num = num+1
+
+f.close()
+of.close()
+
